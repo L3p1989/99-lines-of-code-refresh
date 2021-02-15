6e332177d44beac358e66c567cf3a4988395c91c
L3p1989 <phogan110@live.com>
2021-02-15 16:58:00 -0700

    created double loop that counts down from 99, and uses friends[i] to reference what friend is singing song

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // Array of friends
 let friends = ["Irish", "Siacca", "Glenn"];
+// Loop to make friends array sing 99 lines of code
+for (i = 0; i < friends.length; i++) {
+    // assigns friend to current position in friends array
+    let friend = friends[i];
+    // creates loop that counts down from 99
+    for (n = 99; n > 0; n--) {
+        // assigns minusOne as current loop number - 1
+        let minusOne = n - 1;
+        // if loop number is greater than 0 log song in console
+        if (n > 0) {
+            console.log(`"${n} lines of code in the file, ${n} lines of code; ${friend} strikes one out, clears it all out, ${minusOne} lines of code in the file"`);
+        }
+    }
+};
